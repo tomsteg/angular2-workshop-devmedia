0f19d6f477731353cf8c40d89f0d2513d680efdd
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-05 12:43:33 +0200

    TDD: test first

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -1,10 +1,34 @@
-/* tslint:disable:no-unused-variable */
-
-import { addProviders, async, inject } from '@angular/core/testing';
 import {Book} from './book';
 
 describe('Book', () => {
+
   it('should create an instance', () => {
-    expect(new Book()).toBeTruthy();
+    expect(new Book('1', 'Title', 'Desc')).toBeTruthy();
   });
+
+  describe('rating', () => {
+
+    let book: Book;
+    beforeEach(() => book = new Book('1', 'Title', 'Desc'));
+
+    it('should rate up', () => {
+      book.rating = 1;
+      book.rateUp();
+      expect(book.rating).toEqual(2);
+    });
+
+    it('should not rate higher than 5', () => {
+      book.rating = 5;
+      book.rateUp();
+      expect(book.rating).toEqual(5);
+    });
+
+    it('should not rate lower than 0', () => {
+      book.rating = 0;
+      book.rateDown();
+      expect(book.rating).toEqual(0);
+    });
+
+  });
+
 });
