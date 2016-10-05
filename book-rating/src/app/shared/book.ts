c5cdb51e34fe9f3e12c747f47253f59e6b635b9b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-05 12:49:59 +0200

    TDD: implementation

--- book-rating/src/app/shared/book.ts
+++ book-rating/src/app/shared/book.ts
@@ -6,6 +6,17 @@ export class Book {
     public description: string,
     public rating = 0
     ) {
+  }
 
+  rateUp() {
+    if (this.rating < 5) {
+      this.rating++;
+    }
+  }
+
+  rateDown() {
+    if (this.rating > 0) {
+      this.rating--;
+    }
   }
 }
