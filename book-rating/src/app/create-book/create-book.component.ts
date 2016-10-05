22166384d0e4aee96a12a38e8ef81aeece8f9e75
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-05 16:08:26 +0200

    simple form validation in controller

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -10,7 +10,17 @@ export class CreateBookComponent {
 
   @Output() bookCreated = new EventEmitter<Book>();
 
+  formValid = true;
+
   add(isbn, title, description) {
+
+    if (!isbn.value) {
+      this.formValid = false;
+      return;
+    }
+
+    this.formValid = true;
+
     let book = new Book(isbn.value, title.value, description.value);
     this.bookCreated.emit(book);
     isbn.value = title.value = description.value = ''; // NOCH UNSCHÖN
