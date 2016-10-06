d16f4ac83145c07146ee5e07019caa91946d7fd1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-06 13:06:28 +0200

    resetting the form in component

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Book } from '../shared/book';
 
 @Component({
@@ -8,11 +10,14 @@ import { Book } from '../shared/book';
 })
 export class CreateBookComponent {
 
+  @ViewChild(NgForm) form: NgForm;
   @Output() bookCreated = new EventEmitter<Book>();
   book = Book.empty();
 
   add() {
     this.bookCreated.emit(this.book);
+    this.book = Book.empty();
+    this.form.reset();
     this.book = Book.empty();
   }
 }
