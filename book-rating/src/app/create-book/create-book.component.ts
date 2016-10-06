189133473a5a5a4188a5ff567f0cb7616c3d1713
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-06 10:43:43 +0200

    validation: stylings forms with CSS

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
